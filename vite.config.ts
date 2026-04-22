fb3c8d2a042a78cf962188efc2afbb5aa5cb39ba
dthcamhong99-code <dthcamhong99@gmail.com>
2026-04-22 08:56:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './',
+    base: '/tracuuphapluatdauthau/',
     build: {
       assetsInlineLimit: 100000000, // Force inline all assets (100MB limit)
     },
